a1d2999f0c3e4ab3e5601f976eb77d14c4c642a4
JParker31310 <147316332+JParker31310@users.noreply.github.com>
2024-09-05 09:03:56 -0500

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -124,11 +124,11 @@
 		</ul>
 	</div>
 	<!-- PLEASE DO NOT REMOVE THE COPYRIGHT -->
-	<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/JParker31310/league-page/tree/master">League Page</a></span>
+	//<span class="copyright">&copy; 2021 - {year} <a href="https://github.com/JParker31310/league-page/tree/master">League Page</a></span>
 	<br />
 	<!-- PLEASE DO NOT REMOVE THE BUILT BY -->
-	<span class="creator">Built by <a href="linkedin.com/in/joshua-parker-762518237">Joshua Parker</a><br /></span>
+	//<span class="creator">Built by <a href="linkedin.com/in/joshua-parker-762518237">Joshua Parker</a><br /></span>
 	<!-- You can remove the donation link (although any donations to help
 	 maintain and enhance League Page would be greatly appreciated!) -->
-	Love League Page? Please consider <a href="https://www.venmo.com/u/JoshuaParker13">donating</a> to support enhancements or just to say thank you!
+	//Love League Page? Please consider <a href="https://www.venmo.com/u/JoshuaParker13">donating</a> to support enhancements or just to say thank you!
 </footer>
