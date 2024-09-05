d925e1a8c2232512402c99a7bc76250b9dfa6f78
JParker31310 <147316332+JParker31310@users.noreply.github.com>
2024-09-05 09:41:34 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -88,7 +88,7 @@ export const managers = [
        "tradingScale": 10, // 1 - 10
        "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
      },
-  ]
+  
   
   
   /*   !!  !!  IMPORTANT  !!  !! */
@@ -121,4 +121,5 @@ export const managers = [
     //   "tradingScale": 10, // 1 - 10 (optional)
     //   "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
      },
+  ]
     
