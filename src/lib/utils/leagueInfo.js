0c6d7d0f022f54814c330c321a40feb5264a44a7
JParker31310 <147316332+JParker31310@users.noreply.github.com>
2024-09-05 08:25:48 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "100"; // your league ID
+export const leagueID = "1133971361325760512"; // your league ID
 export const leagueName = "Texas Nation League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
